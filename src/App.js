7f55605ac8020d4af29ac517e4e76f7079d5bf49
Tegan Rauh <onlysayinghi@gmail.com>
2020-02-08 15:51:09 -0600

    Dim counter background when starting to leave view

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import Counter from './Counter';
+import { useIntersect } from './custom-hooks';
 
 const App = () => {
+  const [ref, entry] = useIntersect({});
+
   return (
     <div>
-      <div className="counter-container">
+      <div className={`counter-container ${entry.isIntersecting ? 'dim' : ''}`}>
         <h1>React Hook Dojo</h1>
 
         <Counter />
       </div>
 
-      <div className="out-of-view">
+      <div className="out-of-view" ref={ref}>
         <h2>Hello World</h2>
       </div>
     </div>
